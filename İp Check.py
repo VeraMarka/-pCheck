c0b8da60816147732d5104a69857d1577d33bc80
VeraMarka <165566710+VeraMarka@users.noreply.github.com>
2024-03-31 18:47:20 +0300

    Update İp Check.py

--- İp Check.py
+++ İp Check.py
@@ -1,31 +1,30 @@
-import requests
+import socket
 
-def query_ip():
-    ip = input("IP adresini girin: ")
-    if not ip:
-        print("Geçersiz IP adresi!")
-        return
-    
-    try:
-        response = requests.get(f"http://ip-api.com/json/{ip}")
-        data = response.json()
-        
-        if data["status"] == "fail":
-            print("IP sorgusu başarısız oldu.")
-        else:
-            print(f"""
-                IP Adresi: {data["query"]}
-                Ülke: {data["country"]}
-                Şehir: {data["city"]}
-                Konum: {data["lat"]}, {data["lon"]}
-                ISP: {data["isp"]}
-            """)
-    except Exception as e:
-        print("Bir hata oluştu:", e)
+def get_ip_address(url):
+    ip_address = socket.gethostbyname(url)
+    return ip_address
 
-print("\033[91m" + "İP CHECK")
-query_ip()
+def print_title():
+    print("\033[1m  ______           _              __        __   _")
+    print(" |__  (_)_ __   __| | __ _ _ __   \ \      / /__| |__")
+    print("   / /| | '_ \ / _` |/ _` | '_ \   \ \ /\ / / _ \ '_ \ ")
+    print("  / /_| | | | | (_| | (_| | | | |   \ V  V /  __/ |_) |")
+    print(" /____|_|_| |_|\__,_|\__,_|_| |_|    \_/\_/ \___|_.__/ \n")
 
-print("\nMade By Veraildez")
-print("Instagram: veraildez_v33")
-print("İP CHECK")
+def print_yellow(text):
+    print("\033[93m" + text)
+
+def main_menu():
+    print_title()
+    print_yellow("1- IP adresini bul")
+
+    choice = input("\nHangi seçeneği kullanmak istersiniz (1): ")
+    if choice == "1":
+        website_url = input("\nIP adresini bulmak istediğiniz web sitesinin URL'sini girin: ")
+        ip_address = get_ip_address(website_url)
+        print_yellow("\nGirdiğiniz web sitesinin IP adresi: " + ip_address)
+    else:
+        print_red("\nGeçersiz seçenek! Lütfen 1'i seçin.")
+
+main_menu()
+clear
